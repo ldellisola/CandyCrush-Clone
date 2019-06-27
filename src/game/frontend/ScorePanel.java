51b635f7b7567b39fc0830d89e6ec4cf628ce4aa
Lucas Dell'Isola <ldellisola@itba.edu.ar>
2019-06-27 17:36:45 -0300

    Added a few words to the Score

--- src/game/frontend/ScorePanel.java
+++ src/game/frontend/ScorePanel.java
@@ -10,14 +10,14 @@ public class ScorePanel extends BorderPane {
 
 	public ScorePanel() {
 		setStyle("-fx-background-color: #5490ff");
-		scoreLabel = new Label("0");
+		scoreLabel = new Label("Score: 0");
 		scoreLabel.setAlignment(Pos.CENTER);
 		scoreLabel.setStyle("-fx-font-size: 24");
 		setCenter(scoreLabel);
 	}
 	
 	public void updateScore(String text) {
-		scoreLabel.setText(text);
+		scoreLabel.setText(String.format("Score: %s",text));
 	}
 
 }
